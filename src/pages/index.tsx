024ac644a2402745d975b16d840d9c8219eaa2f8
Zachery Elliot <notzachery@gmail.com>
2023-04-01 02:41:21 -0500

    fix: index page issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import Layout from "src/components/layout";
 import NoAuthLayout from "src/components/no-auth-layout";
 import { User } from "src/types";
+import { isStaff } from "src/util/permission";
 import { developerRoute } from "src/util/redirects";
 import { withSession } from "src/util/session";
 
@@ -13,43 +15,47 @@ interface Props {
   user?: User;
 }
 
-
 export default function MainPage({ user }: Props) {
   const router = useRouter();
   useEffect(() => {
-    if (user) router.push('/dashboard')
-  })
+    if (user) {
+      if (isStaff(user)) router.push("/dashboard");
+      else router.push("/profile");
+    }
+  });
 
-  return (
+  return !user ? (
     <>
       <div className="w-screen h-screen flex justify-center items-center">
         <div className="p-6 max-w-sm mx-auto bg-slate-900 backdrop-blur-3xl bg-opacity-50 rounded-xl shadow-md items-center space-x-1 backdrop-blur">
           <div className="flex-shrink-0 flex justify-center items-center">
             <Image
-              src={
-                `https://brandlogos.net/wp-content/uploads/2021/11/discord-logo.png`
-              }
+              src={`https://brandlogos.net/wp-content/uploads/2021/11/discord-logo.png`}
               height={56}
               width={56}
               alt="Discord Logo"
             ></Image>
           </div>
           <Link href="/api/auth/login" passHref>
-          <button
-            className="px-6 py-2 text-sm justify-start items-start text-white bg-indigo-500 backdrop-blur-3xl bg-opacity-50 font-bold rounded-full border border-indigo-500 hover:bg-indigo-500 hover:text-gray-50 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-5555dd-200 focus:ring-offset-2"
-          >
-            Login with Discord
-          </button>
+            <button className="px-6 py-2 text-sm justify-start items-start text-white bg-indigo-500 backdrop-blur-3xl bg-opacity-50 font-bold rounded-full border border-indigo-500 hover:bg-indigo-500 hover:text-gray-50 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-5555dd-200 focus:ring-offset-2">
+              Login with Discord
+            </button>
           </Link>
         </div>
+      </div>
+    </>
+  ) : (
+    <>
+      <div className="w-screen h-screen flex justify-center items-center">
+        <h1 className="text-while">Loading...</h1>
       </div>
     </>
   );
 }
 
-MainPage.getLayout = function(page: any) {
-  return <NoAuthLayout>{page}</NoAuthLayout>
-}
+MainPage.getLayout = function (page: any) {
+  return <Layout user={null}>{page}</Layout>;
+};
 
-
-export const getServerSideProps: GetServerSideProps = withSession(developerRoute)
+export const getServerSideProps: GetServerSideProps =
+  withSession(developerRoute);
