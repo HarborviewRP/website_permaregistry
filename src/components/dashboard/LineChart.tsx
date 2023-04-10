bc583742847661fe85fbacdc06e5de75b9ed009f
Zachery Elliot <notzachery@gmail.com>
2023-04-09 21:59:47 -0500

    feat: graph styling

--- src/components/dashboard/LineChart.tsx
+++ src/components/dashboard/LineChart.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef } from 'react';
-import { Line } from 'react-chartjs-2';
-import 'chart.js/auto';
-import { ScriptableContext } from 'chart.js/auto';
+import React, { useEffect, useRef } from "react";
+import { Line } from "react-chartjs-2";
+import "chart.js/auto";
+import { ScriptableContext } from "chart.js/auto";
 
 interface LineChartProps {
   label: string;
@@ -13,34 +13,49 @@ const LineChart: React.FC<LineChartProps> = ({ data, label }) => {
     labels: data.map((item: any) => item.date),
     datasets: [
       {
-        lineTension: 0.2,
         label: label,
         data: data.map((item: any) => item.count),
-        borderColor: 'rgba(136,132,216,0.2',
+        borderColor: "rgba(136,132,216,0.2",
         borderWidth: 2,
         //backgroundColor: 'rgba(136, 132, 216, 0.1)',
         backgroundColor: (context: ScriptableContext<"line">) => {
           const ctx = context.chart.ctx;
-          const gradient = ctx.createLinearGradient(0, 0, 0, 275);
-          gradient.addColorStop(0, "rgba(136,132,216,0.5)");
-          gradient.addColorStop(1, "rgba(136,132,216,0)");
+          const gradient = ctx.createLinearGradient(0, 0, 0, 300);
+          gradient.addColorStop(0, "rgba(89, 86, 148,0.5)");
+          gradient.addColorStop(1, "rgba(89, 86, 148,0)");
           return gradient;
         },
         pointBorderColor: "rgba(136,132,216,0.0)",
-        pointBackgroundColor: 'rgba(136,132,216,0.1)',
-        fill: true
+        pointBackgroundColor: "rgba(89, 86, 148,1)",
+        pointHitRadius: 256,
+        pointRadius: 2.25,
+        pointHoverRadius: 5,
+        fill: true,
+        yAxisID: "y",
+        xAxisID: "x",
       },
     ],
   };
 
   const chartOptions = {
     maintainAspectRatio: false,
+    scales: {
+      y: {
+        ticks: {
+          beginAtZero: true,
+          stepSize: 1,
+        },
+        scaleLabel: {
+          display: true,
+          labelString: "Count",
+        },
+      },
+    },
   };
 
   return (
     <div className="bg-slate-900 backdrop-blur-3xl bg-opacity-50 text-white p-4 rounded-lg">
-
-      <div style={{ width: '100%', height: 300 }}>
+      <div style={{ width: "100%", height: 300 }}>
         <Line data={chartData} options={chartOptions as any} />
       </div>
     </div>
