c70e86da780615a04421ab98b4f43e49f97c9b7a
Zachery Elliot <notzachery@gmail.com>
2023-04-12 03:54:30 -0500

    fix: superadmins are superadmins regardless

--- src/util/permission.ts
+++ src/util/permission.ts
@@ -2,8 +2,9 @@ import { DISCORD, User } from "src/types"
 
 export const isStaff = (user: User) => {
     if (!user) return false;
+    if (user.access_level > 0) return true;
     if (!user.roles) return false;
-    return user.roles.includes(DISCORD.STAFF_ROLE_ID) || user.access_level > 0;
+    return user.roles.includes(DISCORD.STAFF_ROLE_ID);
 }
 
 export const isAdmin = (user: User) => {
